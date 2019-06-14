07348e57d2a58741ce031ed448ee802e1f4568ba
Ying Guo <yinghao.guo34@bcmail.brooklyn.cuny.edu>
2019-06-13 21:23:50 -0400

    log in log out function working.

--- users/views.py
+++ users/views.py
@@ -1,11 +1,1 @@
 from django.contrib.auth.forms import AuthenticationForm
-
-def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(data = request.POST)
-        if form.is_valid():
-
-    else:
-        form = AuthenticationForm()
-
-    return render(request, 'users/login.html' {'form':form})
