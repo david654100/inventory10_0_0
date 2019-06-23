7915ee91bf9998df2f8a68a1280a4d2f77db313a
TestingYG <31413025+TestingYG@users.noreply.github.com>
2019-06-23 16:52:37 -0400

    Update admin.py

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from users.models import account_activity, user_login_information
+from users.models import account_activity, user_login_information, Profile
 
 # Register your models here.
 
+admin.site.register(Profile)
 admin.site.register(account_activity)
 admin.site.register(user_login_information)
